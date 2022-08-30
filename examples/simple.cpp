925d733a21fd6955b50c927709389a1ec641ed68
2bit <i@2bit.jp>
2022-08-31 00:49:20 +0900

    update example

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -10,9 +10,20 @@ int main() {
     receiver->add_callback("/test", [] {
         std::cout << "received /test" << std::endl;
     });
+
+    bbb::osc::sender sender;
+    sender.setup("127.0.0.1", 26666);
+
+    int i = 0;
     while(true) {
         manager.update();
+        if(i % 100 == 0) {
+            sender.send("/test");
+            i = 0;
+        }
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        i++;
     }
+
     return 0;
 }
