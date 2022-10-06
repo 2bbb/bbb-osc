8a9272526fa82077540b82bc2c4ae087e679d018
2bit <i@2bit.jp>
2022-10-06 11:03:12 +0900

    implement manager

--- include/bbb/osc/sender.hpp
+++ include/bbb/osc/sender.hpp
@@ -29,13 +29,14 @@ namespace bbb {
         class sender : public bbb::udp::sender {
         public:
             using bbb::udp::sender::setup;
-            
-            template <typename ... ArgTypes>
+            using endpoint = bbb::udp::sender::endpoint;
+
+            template <typename ... arguments>
             void send(const std::string &address,
-                      ArgTypes && ... args)
+                      arguments && ... args)
             {
                 send(bbb::osc::message(address,
-                                       std::forward<ArgTypes>(args) ...));
+                                       std::forward<arguments>(args) ...));
             }
             
             void send(const std::string &address,
@@ -50,6 +51,74 @@ namespace bbb {
                 std::uint8_t buffer[bbb::udp::buf_size]{0};
                 auto packet_size = mess.packet(buffer, bbb::udp::buf_size);
                 this->udp::sender::send(buffer, packet_size);
+            }
+
+            struct manager final {
+                static manager &shared() {
+                    static manager _;
+                    return _;
+                }
+                
+                std::map<bbb::osc::sender::endpoint, std::shared_ptr<bbb::osc::sender>> senders;
+                
+                template <typename derived_sender = bbb::osc::sender>
+                auto get(const std::string &host, std::uint16_t port)
+                    -> typename std::enable_if<
+                        std::is_base_of<bbb::osc::sender, derived_sender>::value,
+                        std::shared_ptr<derived_sender>
+                    >::type
+                {
+                    bbb::osc::sender::endpoint endpoint{host, port};
+                    auto it = senders.find(endpoint);
+                    if(it != senders.end()) {
+                        return std::dynamic_pointer_cast<derived_sender>(it->second);
+                    }
+                    
+                    auto sender = std::make_shared<derived_sender>();
+                    senders.insert(std::make_pair(std::move(endpoint), sender));
+                    return sender;
+                }
+                
+                template <typename ... arguments>
+                inline void send(const std::string &host,
+                                 std::uint16_t port,
+                                 const std::string &address,
+                                 arguments && ... args)
+                {
+                    get(host, port)->send(bbb::osc::message(address,
+                                          std::forward<arguments>(args) ...));
+                }
+                
+                inline void send(const std::string &host,
+                                 std::uint16_t port,
+                                 const std::string &address,
+                                 bbb::osc::message mess)
+                {
+                    get(host, port)->send(mess);
+                }
+                
+                inline void send(const std::string &host,
+                                 std::uint16_t port,
+                                 const bbb::osc::message &mess)
+                {
+                    get(host, port)->send(mess);
+                }
+
+            private:
+                manager() {};
+                manager(const manager &) = delete;;
+                manager &operator=(const manager &) = delete;
+                friend class bbb::osc::sender;
+            }; // struct manager
+
+            template <typename derived_sender = bbb::osc::sender>
+            static auto get(const std::string &host, std::uint16_t port)
+                -> typename std::enable_if<
+                    std::is_base_of<bbb::osc::sender, derived_sender>::value,
+                    std::shared_ptr<derived_sender>
+                >::type
+            {
+                return manager::shared().get<derived_sender>(host, port);
             }
         }; // struct sender
     }; // namespace osc
